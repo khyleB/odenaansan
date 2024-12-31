37c4b150bd43c86f4dbe301c545115b03ac33cf1
KhyleB <khyle.best@outlook.com>
2024-12-31 02:16:41 -0500

    Touched up about us page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Index */
+
 let blob = document.querySelectorAll('.text-blob-container');
 
 window.addEventListener('DOMContentLoaded', ( ) => {
@@ -12,3 +14,15 @@ window.addEventListener('DOMContentLoaded', ( ) => {
 
     });
 })
+
+
+
+/* About */
+
+function showBio() {
+	let bioText = $(this)
+	let bioClass = $(bio)
+
+	$("bioText > bioClass").removeClass(hidden);
+}
+
